3502ba01db2aff13e135a648fee0158abae1257b
Damian Stachura <damian.stachura@evidenceprime.com>
2025-12-16 16:01:02 +0100

    Moved inspect converter scripts from EvalHub repo to EEE

---
+++ scripts/eval_converters/inspect/adapter.py
@@ -0,0 +1,153 @@
+import os
+
+from inspect_ai.log import EvalLog, EvalSpec, EvalStats, read_eval_log
+
+from pathlib import Path
+from typing import Dict, List, Union
+
+from eval_types import (
+    DetailedEvaluationResultsPerSample,
+    EvaluationLog,
+    EvaluationResult,
+    MetricConfig,
+    ModelInfo,
+    ScoreDetails,
+    SourceData,
+    SourceMetadata
+)
+
+from scripts.eval_converters.common.adapter import AdapterMetadata, BaseEvaluationAdapter, SupportedLibrary
+from scripts.eval_converters.common.error import AdapterError
+from scripts.eval_converters.common.utils import convert_timestamp_to_unix_format
+from scripts.eval_converters.inspect.utils import extract_model_info_from_model_path
+
+SCHEMA_VERSION = '0.0.1'
+
+class InspectAIAdapter(BaseEvaluationAdapter):
+    """
+    Adapter for transforming evaluation outputs from the Inspect AI library into the unified schema format.
+    """
+
+    @property
+    def metadata(self) -> AdapterMetadata:
+        return AdapterMetadata(
+			name="InspectAdapter",
+			version="0.0.1",
+			description="Adapter for transforming HELM evaluation outputs to unified schema format"
+		)
+
+    @property
+    def supported_library(self) -> SupportedLibrary:
+        return SupportedLibrary.INSPECT_AI
+        
+    def transform_from_directory(self, dir_path: Union[str, Path]):
+        raise NotImplementedError("Inspect AI adapter do not support loading logs from directory!")
+
+    def transform_from_file(self, file_path: Union[str, Path], metadata_args: Dict = None) -> Union[EvaluationLog, List[EvaluationLog]]:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f'File path {file_path} does not exists!')
+        
+        try:
+            file_path = Path(file_path) if isinstance(file_path, str) else file_path
+            eval_log: EvalLog = self._load_file(file_path)
+            return self.transform(eval_log, metadata_args)
+        except AdapterError as e:
+            raise e
+        except Exception as e:
+            raise AdapterError(f"Failed to load file {file_path}: {str(e)} for InspectAIAdapter")
+
+    def _transform_single(self, raw_data: EvalLog, metadata_args: Dict) -> EvaluationLog:
+        eval_spec: EvalSpec = raw_data.eval
+        eval_stats: EvalStats = raw_data.stats
+
+        retrieved_timestamp = eval_stats.started_at or eval_spec.created
+        retrieved_unix_timestamp = convert_timestamp_to_unix_format(retrieved_timestamp)
+        
+        source_data = SourceData(
+            dataset_name=eval_spec.dataset.name.split('/')[-1],
+            hf_repo=eval_spec.dataset.location,
+            samples_number=eval_spec.dataset.samples,
+            sample_ids=eval_spec.dataset.sample_ids
+        )
+
+        source_metadata = SourceMetadata(
+            source_name='inspect_ai',
+            source_type='evaluation_run',
+            source_organization_name=metadata_args.get('source_organization_name'),
+            source_organization_url=metadata_args.get('source_organization_url'),
+            source_organization_logo_url=metadata_args.get('source_organization_logo_url'),
+            evaluator_relationship=metadata_args.get('evaluator_relationship')
+        )
+
+        model_path = eval_spec.model
+        self._check_if_model_is_on_huggingface(model_path)
+
+        if raw_data.samples:
+            detailed_model_name = raw_data.samples[0].output.model
+            model_path_parts = model_path.split('/')
+
+            if model_path_parts[-1] in detailed_model_name:
+                model_path_parts[-1] = detailed_model_name
+
+            model_path = '/'.join(model_path_parts)
+
+        model_info: ModelInfo = extract_model_info_from_model_path(model_path)
+
+        results = raw_data.results
+        evaluation_results = []
+
+        generation_config = {
+            gen_config: value 
+            for gen_config, value in vars(eval_spec.model_generate_config).items() if value is not None
+        }
+
+        for scorer_results in results.scores:
+            scorer_name = scorer_results.scorer
+            for metric in scorer_results.metrics:
+                metric_info = scorer_results.metrics[metric]
+                if metric_info.name != 'stderr':
+                    evaluation_results.append(EvaluationResult(
+                        evaluation_name=scorer_name,
+                        evaluation_timestamp=convert_timestamp_to_unix_format(eval_stats.completed_at),
+                        metric_config=MetricConfig(
+                            evaluation_description=metric_info.name,
+                            lower_is_better=False # probably there is no access to such info
+                        ),
+                        score_details=ScoreDetails(
+                            score=metric_info.value
+                        ),
+                        generation_config=generation_config
+                    ))
+
+        detailed_evaluation_results_per_samples = []
+        for sample in raw_data.samples:
+            if sample.scores:
+                response = sample.scores.get('choice').answer
+            else:
+                response = sample.output.choices[0].message.content
+
+            detailed_evaluation_results_per_samples.append(
+                DetailedEvaluationResultsPerSample(
+                    sample_id=str(sample.id),
+                    input=sample.input,
+                    ground_truth=sample.target,
+                    response=response,
+                    choices=sample.choices
+                )
+            )
+
+        evaluation_id = f'inspect_ai/{model_path}/{eval_spec.dataset.name}/{retrieved_unix_timestamp}'
+
+        return EvaluationLog(
+            schema_version=SCHEMA_VERSION,
+            evaluation_id=evaluation_id,
+            retrieved_timestamp=retrieved_unix_timestamp,
+            source_data=source_data,
+            source_metadata=source_metadata,
+            model_info=model_info,
+            evaluation_results=evaluation_results,
+            detailed_evaluation_results_per_samples=detailed_evaluation_results_per_samples
+        )
+        
+    def _load_file(self, file_path) -> EvalLog:
+        return read_eval_log(file_path)
---
+++ tests/test_inspect_adapter.py
@@ -0,0 +1,165 @@
+from pathlib import Path
+
+from scripts.eval_converters.inspect.adapter import InspectAIAdapter
+from scripts.eval_converters.inspect.utils import extract_model_info_from_model_path
+from eval_types import (
+    EvaluationLog, 
+    EvaluatorRelationship,
+    SourceData,
+    SourceMetadata,
+
+)
+
+def _load_eval(adapter, filepath, source_metadata):
+    eval_path = Path(filepath)
+    converted_eval = adapter.transform_from_file(eval_path, source_metadata=source_metadata)
+    assert isinstance(converted_eval, EvaluationLog)
+    assert isinstance(converted_eval.source_data, SourceData)
+
+    assert converted_eval.source_metadata.source_name == 'inspect_ai'
+    assert converted_eval.source_metadata.source_type == 'evaluation_run'
+
+    return converted_eval
+
+
+def test_pubmedqa_eval():
+    adapter = InspectAIAdapter()
+    source_metadata = SourceMetadata(
+        source_name='inspect_ai',
+        source_type='evaluation_run',
+        source_organization_name='TestOrg',
+        evaluator_relationship=EvaluatorRelationship.first_party,
+    )
+
+    converted_eval = _load_eval(adapter, 'tests/data/inspect/data_pubmedqa_gpt4o_mini.json', source_metadata)
+
+    assert converted_eval.retrieved_timestamp == '1751553870.0'
+    
+    assert converted_eval.source_data.dataset_name == 'pubmed_qa'
+    assert converted_eval.source_data.hf_repo == 'bigbio/pubmed_qa'
+    assert len(converted_eval.source_data.sample_ids) == 2
+
+    assert converted_eval.model_info.name == 'openai/azure/gpt-4o-mini-2024-07-18'
+    assert converted_eval.model_info.id == 'openai/gpt-4o-mini-2024-07-18'
+    assert converted_eval.model_info.developer == 'openai'
+    assert converted_eval.model_info.inference_platform == 'azure'
+    assert converted_eval.model_info.inference_engine is None
+
+    results = converted_eval.evaluation_results
+    assert results[0].evaluation_name == 'choice'
+    assert results[0].metric_config.evaluation_description == 'accuracy'
+    assert results[0].score_details.score == 1.0
+
+    results_per_sample = converted_eval.detailed_evaluation_results_per_samples
+    sample_ids = [sample.sample_id for sample in results_per_sample]
+    assert sorted(sample_ids) == ['12377809', '26163474']
+    assert results_per_sample[0].ground_truth == 'A'
+    assert results_per_sample[0].response == 'A'
+    assert results_per_sample[0].choices == ['yes', 'no', 'maybe']
+
+
+def test_arc_sonnet_eval():
+    adapter = InspectAIAdapter()
+    source_metadata = SourceMetadata(
+        source_name='inspect_ai',
+        source_type='evaluation_run',
+        source_organization_name='TestOrg',
+        evaluator_relationship=EvaluatorRelationship.first_party,
+    )
+
+    converted_eval = _load_eval(adapter, 'tests/data/inspect/data_arc_sonnet.json', source_metadata)
+
+    assert converted_eval.retrieved_timestamp == '1761000045.0'
+
+    assert converted_eval.source_data.dataset_name == 'ai2_arc'
+    assert converted_eval.source_data.hf_repo == 'allenai/ai2_arc'
+    assert len(converted_eval.source_data.sample_ids) == 5
+
+    assert converted_eval.model_info.name == 'anthropic/claude-sonnet-4-0'
+    assert converted_eval.model_info.id == 'anthropic/claude-sonnet-4-0'
+    assert converted_eval.model_info.developer == 'anthropic'
+    assert converted_eval.model_info.inference_platform == 'anthropic'
+    assert converted_eval.model_info.inference_engine is None
+
+    results = converted_eval.evaluation_results
+    assert results[0].evaluation_name == 'choice'
+    assert results[0].metric_config.evaluation_description == 'accuracy'
+    assert results[0].score_details.score == 1.0
+
+    results_per_sample = converted_eval.detailed_evaluation_results_per_samples
+    sample_ids = [sample.sample_id for sample in results_per_sample]
+    assert sorted(sample_ids) == ['1', '2', '3', '4', '5']
+    assert results_per_sample[0].ground_truth == 'A'
+    assert results_per_sample[0].response == 'A'
+    assert 'Sunlight is the source of energy for nearly all ecosystems.' in results_per_sample[0].choices
+
+
+def test_arc_qwen_eval():
+    adapter = InspectAIAdapter()
+    source_metadata = SourceMetadata(
+        source_name='inspect_ai',
+        source_type='evaluation_run',
+        source_organization_name='TestOrg',
+        evaluator_relationship=EvaluatorRelationship.first_party,
+    )
+
+    converted_eval = _load_eval(adapter, 'tests/data/inspect/data_arc_qwen.json', source_metadata)
+
+    assert converted_eval.retrieved_timestamp == '1761001924.0'
+
+    assert converted_eval.source_data.dataset_name == 'ai2_arc'
+    assert converted_eval.source_data.hf_repo == 'allenai/ai2_arc'
+    assert len(converted_eval.source_data.sample_ids) == 3
+
+    assert converted_eval.model_info.name == 'ollama/qwen2.5:0.5b'
+    assert converted_eval.model_info.id == 'ollama/qwen2.5-0.5b'
+    assert converted_eval.model_info.developer == 'ollama'
+    assert converted_eval.model_info.inference_platform is None
+    assert converted_eval.model_info.inference_engine == 'ollama'
+
+    results = converted_eval.evaluation_results
+    assert results[0].evaluation_name == 'choice'
+    assert results[0].metric_config.evaluation_description == 'accuracy'
+    assert results[0].score_details.score == 0.3333333333333333
+
+    results_per_sample = converted_eval.detailed_evaluation_results_per_samples
+    sample_ids = [sample.sample_id for sample in results_per_sample]
+    assert sorted(sample_ids) == ['1', '2', '3']
+    assert results_per_sample[1].ground_truth == 'B'
+    assert results_per_sample[1].response == 'D'
+    assert results_per_sample[1].choices == ["safety goggles", "breathing mask", "rubber gloves", "lead apron"]
+
+def test_convert_model_path_to_standarized_model_ids():
+    model_path_to_standarized_id_map = {
+        "openai/gpt-4o-mini": "openai/gpt-4o-mini",
+        "openai/azure/gpt-4o-mini": "openai/gpt-4o-mini",
+        "anthropic/claude-sonnet-4-0": "anthropic/claude-sonnet-4-0",
+        "anthropic/bedrock/anthropic.claude-3-5-sonnet-20241022-v2:0": "anthropic/claude-3-5-sonnet@20241022",
+        "anthropic/vertex/claude-3-5-sonnet-v2@20241022": "anthropic/claude-3-5-sonnet@20241022",
+        "google/gemini-2.5-pro": "google/gemini-2.5-pro",
+        "google/vertex/gemini-2.0-flash": "google/gemini-2.0-flash",
+        "mistral/mistral-large-latest": "mistral/mistral-large-latest",
+        "mistral/azure/Mistral-Large-2411": "mistral/Mistral-Large-2411",
+        "openai-api/deepseek/deepseek-reasoner": "deepseek/deepseek-reasoner",
+        "bedrock/meta.llama2-70b-chat-v1": "meta/llama2-70b-chat",
+        "azureai/Llama-3.3-70B-Instruct": "azureai/Llama-3.3-70B-Instruct",
+        "together/meta-llama/Meta-Llama-3.1-70B-Instruct": "meta-llama/Meta-Llama-3.1-70B-Instruct",
+        "groq/llama-3.1-70b-versatile": "meta-llama/llama-3.1-70b-versatile",
+        "fireworks/accounts/fireworks/models/deepseek-r1-0528": "deepseek-ai/deepseek-r1-0528",
+        "sambanova/DeepSeek-V1-0324": "deepseek-ai/DeepSeek-V1-0324",
+        "cf/meta/llama-3.1-70b-instruct": "meta/llama-3.1-70b-instruct",
+        "perplexity/sonar": "perplexity/sonar",
+        "hf/openai-community/gpt2": "openai-community/gpt2",
+        "vllm/openai-community/gpt2": "openai-community/gpt2",
+        "vllm/meta-llama/Meta-Llama-3-8B-Instruct": "meta-llama/Meta-Llama-3-8B-Instruct",
+        "sglang/meta-llama/Meta-Llama-3-8B-Instruct": "meta-llama/Meta-Llama-3-8B-Instruct",
+        "ollama/llama3.1": "ollama/llama3.1",
+        "llama-cpp-python/llama3": "llama-cpp-python/llama3",
+        "openrouter/gryphe/mythomax-l2-13b": "gryphe/mythomax-l2-13b",
+        "hf-inference-providers/openai/gpt-oss-120b": "openai/gpt-oss-120b",
+        "hf-inference-providers/openai/gpt-oss-120b:cerebras": "openai/gpt-oss-120b:cerebras",
+    }
+
+    for model_path, model_id in model_path_to_standarized_id_map.items():
+        model_info = extract_model_info_from_model_path(model_path)
+        assert model_info.id == model_id
